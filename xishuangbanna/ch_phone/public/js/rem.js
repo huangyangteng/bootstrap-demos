99ed1223bf905346e062853730c779e9d3901c08
yellowyellow <yellowyellow12138@gmail.com>
2018-07-15 15:52:58 +0800

    光猪项目初始化

---
+++ xishuangbanna/ch_phone/public/js/rem.js
@@ -0,0 +1,18 @@
+(function () {
+    // 基准大小
+    var baseSize = 16;
+    // 设置 rem 函数
+    function setRem() {
+        // 当前页面宽度相对于 750 宽的缩放比例，可根据自己需要修改。
+        var scale = document.documentElement.clientWidth / 375;
+        // 设置页面根节点字体大小
+        document.documentElement.style.fontSize = (baseSize * Math.min(scale, 5)) + 'px'
+    }
+    // 初始化
+    setRem();
+    // 改变窗口大小时重新设置rem
+    window.onresize = function () {
+        setRem()
+    }
+    console.log(document.documentElement.style.fontSize)
+}());
