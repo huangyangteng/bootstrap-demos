92c8a86478feb5bce14ef8565458c7135f3d5618
yellowyellow <yellowyellow12138@gmail.com>
2018-07-16 00:07:59 +0800

    处理好移动端适配问题

--- xishuangbanna/ch_phone/public/js/rem.js
+++ xishuangbanna/ch_phone/public/js/rem.js
@@ -3,8 +3,8 @@
     var baseSize = 16;
     // 设置 rem 函数
     function setRem() {
-        // 当前页面宽度相对于 640 宽的缩放比例，可根据自己需要修改。
-        var scale = document.documentElement.clientWidth / 640;
+        // 当前页面宽度相对于 320 宽的缩放比例，可根据自己需要修改。
+        var scale = document.documentElement.clientWidth / 420;
         // 设置页面根节点字体大小
         document.documentElement.style.fontSize = (baseSize * Math.min(scale, 5)) + 'px'
     }
