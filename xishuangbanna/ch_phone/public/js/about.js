04722eddd08a857ee9f3e202d0141c611cad2b14
yellowyellow <yellowyellow12138@gmail.com>
2018-07-17 18:54:52 +0800

    加盟合作页面暂停

---
+++ xishuangbanna/ch_phone/public/js/about.js
@@ -0,0 +1,11 @@
+// 加盟合作页面轮播
+(function () {
+    new Swiper('#join-banner', {
+        // autoplay: true,
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    })     
+}())
