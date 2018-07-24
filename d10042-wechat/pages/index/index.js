a9c7561b08d38f346fbab6f076737406fe548726
yellowyellow <yellowyellow12138@gmail.com>
2018-07-24 17:01:42 +0800

    微信项目初始化

---
+++ d10042-wechat/pages/index/index.js
@@ -0,0 +1,17 @@
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+
+  },
+  onShareAppMessage: function () {
+    // 用户点击右上角分享
+    return {
+      title: '自定义', // 分享标题
+      desc: '啦啦啦啦啦', // 分享描述
+      path: 'path' // 分享路径
+    }
+  }
+})
