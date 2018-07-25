991ec2f03b02ce132de9dc4a8a0ed7e27df1dce5
yellowyellow <yellowyellow12138@gmail.com>
2018-07-25 10:51:51 +0800

    完成消息

--- d10042-wechat/pages/dynamic/dynamic.js
+++ d10042-wechat/pages/dynamic/dynamic.js
@@ -1,6 +1,58 @@
 Page({
     data: {
-
+        dynamics:[
+            {
+                name:'nice',
+                avatar:'../imgs/icon/avatar.png',
+                tags:[
+                    '22岁',
+                    '175cm',
+                    '本科'
+                ],
+                tag_imgs:[
+                    '../imgs/icon/car.png',
+                    '../imgs/icon/room.png',
+                ],
+                dynamic: '希望在这里遇到自己喜欢的然后结婚过一辈子，我知道塑造自己过程很痛快哭，但是最终会收获一个更好的自己，也会把一个更好的自己带给另一半。',
+                time:'07-12',
+                comments_num:1,
+                stars_num:10,
+            },
+            {
+                name:'南方',
+                avatar:'../imgs/icon/avatar.png',
+                tags:[
+                    '22岁',
+                    '175cm',
+                    '本科'
+                ],
+                tag_imgs:[
+                    '../imgs/icon/car.png',
+                    '../imgs/icon/room.png',
+                ],
+                dynamic: '希望在这里遇到自己喜欢的然后结婚过一辈子，我知道塑造自己过程很痛快哭，但是最终会收获一个更好的自己，也会把一个更好的自己带给另一半。',
+                time:'07-12',
+                comments_num:1,
+                stars_num:10,
+            },
+            {
+                name:'南方',
+                avatar:'../imgs/icon/avatar.png',
+                tags:[
+                    '22岁',
+                    '175cm',
+                    '本科'
+                ],
+                tag_imgs:[
+                    '../imgs/icon/car.png',
+                    '../imgs/icon/room.png',
+                ],
+                dynamic: '希望在这里遇到自己喜欢的然后结婚过一辈子，我知道塑造自己过程很痛快哭，但是最终会收获一个更好的自己，也会把一个更好的自己带给另一半。',
+                time:'07-12',
+                comments_num:1,
+                stars_num:10,
+            },
+        ],
     },
     onLoad: function (options) {
         // 生命周期函数--监听页面加载
