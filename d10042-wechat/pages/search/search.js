2a469f6db832f0f983d230248f24462aa96d915a
yellowyellow <yellowyellow12138@gmail.com>
2018-07-27 19:54:34 +0800

    完成搜索

---
+++ d10042-wechat/pages/search/search.js
@@ -0,0 +1,25 @@
+Page({
+    data: {
+        array: [
+            '测试数据1', '测试数据2', '测试数据3', '测试数据4'
+        ],
+        index: 0
+    },
+    onLoad: function (options) {
+        // 生命周期函数--监听页面加载
+
+    },
+    selectTest(e) {
+        this.setData({
+            index: e.detail.value
+        })
+    },
+    onShareAppMessage: function () {
+        // 用户点击右上角分享
+        return {
+            title: '自定义', // 分享标题
+            desc: '啦啦啦啦啦', // 分享描述
+            path: 'path' // 分享路径
+        }
+    }
+})
