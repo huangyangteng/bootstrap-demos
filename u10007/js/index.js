cebf95f23031504673e245b1def6e2181bea15eb
yellowyellow <yellowyellow12138@gmail.com>
2018-08-14 16:06:40 +0800

    增加选项卡逻辑

--- u10007/js/index.js
+++ u10007/js/index.js
@@ -2,15 +2,6 @@
 
 // -------------辅助js 可删除
 
-//如果a标签的href属性为空，那么设置为javascript::
-var all_link = Array.prototype.slice.apply($('a'));
-all_link.forEach(item => {
-    if(item.href==''){
-        item.href = 'javascript:;'
-    }
-});
-
-
 
 
 
@@ -26,7 +17,7 @@ new Swiper('.swiper-container', {
 })
 // process-step1 轮播
 new Swiper('#step1-banner', {
-    autoplay: true,
+    // autoplay: true,
     direction: 'horizontal',
     loop: true,
     pagination: {
@@ -56,7 +47,33 @@ new Swiper('#step3-banner', {
 })
 
 
-// toggle panel
+// menu toggle
+var menus=$('#index-menu').find('a'),
+    menu_content = $('#menu-content');
+
+// 点击时，显示点击的那个，隐藏其他的
+menus.on('click',function () {
+    var _this=$(this),
+        target_flag=_this.attr('id');
+    var target = menu_content.find(`.${target_flag}`),
+        siblings=target.siblings();
+    // 显示隐藏切换
+    siblings.addClass('hidden');
+    target.removeClass('hidden');
+
+    // 样式切换
+    _this.addClass('active')
+    .siblings().removeClass('active');
+
+})
+
+
+
+
+
+
+
+//process toggle panel
 var toggle_panel_btns = $('.toggle-panel');
 var panels = $('.panel');
 toggle_panel_btns.click(function (e) {
@@ -85,3 +102,5 @@ toggle_panel_btns.click(function (e) {
         arrow.addClass('js-reverse');
     }
 })
+
+
